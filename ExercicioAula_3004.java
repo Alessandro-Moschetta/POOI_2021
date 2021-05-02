82211e833715b9eb3bfef8a944811bfd9ca2bdae
Alessandro-Moschetta <73263891+Alessandro-Moschetta@users.noreply.github.com>
2021-05-02 18:42:08 -0300

    Add files via upload

---
+++ ExercicioAula_3004.java
@@ -0,0 +1,11 @@
+package exercicioaula_3004;
+
+public class ExercicioAula_3004 {
+
+    public static void main(String[] args) {
+        
+        LigaDesligaRadio m1 = new LigaDesligaRadio();
+        
+        m1.status();
+    }    
+}
