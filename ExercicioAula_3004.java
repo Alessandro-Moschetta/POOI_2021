e856432d2e76f41367d57e3a688e597751167282
Alessandro-Moschetta <73263891+Alessandro-Moschetta@users.noreply.github.com>
2021-05-07 19:34:36 -0300

    Add files via upload

--- ExercicioAula_3004.java
+++ ExercicioAula_3004.java
@@ -4,8 +4,31 @@ public class ExercicioAula_3004 {
 
     public static void main(String[] args) {
         
-        LigaDesligaRadio m1 = new LigaDesligaRadio();
+        LigaDesligaEstacaoRadio r1 = new LigaDesligaEstacaoRadio();
         
-        m1.status();
+        //r1.ligaRadio(); // foi de 0 para 2
+        //r1.ligaRadio(); // foi de 2 para 4
+        //r1.ligaRadio(); // foi de 4 para 6
+        //r1.ligaRadio(); // foi de 6 para 8
+        //r1.ligaRadio(); // foi de 8 para 10
+        //r1.ligaRadio(); // foi de 10 para 12
+        //r1.status();
+        //System.out.println("Posição: " +r1.getPosicao());
+        
+        //r1.estacaoconectadaRadio();
+        //r1.estacaoconectadaRadio();
+        //r1.status();
+        
+        //r1.estacaodesconectadaRadio();
+        //r1.estacaodesconectadaRadio();
+        //r1.status();
+        
+        //r1.desligaRadio();
+        //System.out.println("Posição: " +r1.getPosicao());
+        //r1.status();
+        
+        //r1.setPosicao(10);
+        //System.out.println("Posição: " +r1.getPosicao());
+        r1.status();
     }    
 }
